269d00d04aba6bcde5ffd34230e74f0fc6b2a281
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2025-02-12 23:25:46 -0400

    fix: validating children errors in validation exceptions

--- src/config/exceptions/class-validator.exception.ts
+++ src/config/exceptions/class-validator.exception.ts
@@ -18,10 +18,23 @@ export function ClassValidatorExceptions(errors: ValidationError[]) {
   const e = errors.map((error) => {
     const tmp = {};
     tmp[error.property] = [];
-    const keys = Object.keys(error.constraints);
-    keys.forEach((key) => {
-      tmp[error.property].push(error.constraints[key]);
-    });
+    let keys = null;
+    if (error.children.length > 0) {
+      error.children.forEach((children) => {
+        const childErrors = {};
+        childErrors[children.property] = [];
+        keys = Object.keys(children.constraints);
+        keys.forEach((key) => {
+          childErrors[children.property].push(children.constraints[key]);
+        });
+        tmp[error.property].push(childErrors);
+      });
+    } else {
+      keys = Object.keys(error.constraints);
+      keys.forEach((key) => {
+        tmp[error.property].push(error.constraints[key]);
+      });
+    }
     return tmp;
   });
 
