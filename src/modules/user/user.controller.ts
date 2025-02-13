6d3bd9f9dda9db07977bd410c4957dc2d396ce8f
Luis A. Alvarado Z <luis_alejandro.az@outlook.com>
2025-02-13 02:37:38 -0400

    feat: adding auth jwt to user routes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,11 +8,14 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  Req,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ParseMongoIdPipe } from '@src/shared';
+import { FastifyRequest } from 'fastify';
+import { AuthAll } from '../auth';
 
 @Controller('user')
 export class UserController {
@@ -24,27 +27,35 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @AuthAll()
   @Get()
   @HttpCode(HttpStatus.OK)
   findAll() {
     return this.userService.findAll();
   }
 
+  @AuthAll()
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   findOne(@Param('id', new ParseMongoIdPipe()) id: string) {
     return this.userService.findOne(id);
   }
 
+  @AuthAll()
   @Patch(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  update(
-    @Param('id', new ParseMongoIdPipe()) id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    return this.userService.update(id, updateUserDto);
+  update(@Req() req: FastifyRequest, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(req['user']._id, updateUserDto);
   }
 
+  @AuthAll()
+  @Delete('delete-me')
+  @HttpCode(HttpStatus.ACCEPTED)
+  removeUser(@Req() req: FastifyRequest) {
+    return this.userService.remove(req['user']._id);
+  }
+
+  @AuthAll()
   @Delete(':id')
   @HttpCode(HttpStatus.ACCEPTED)
   remove(@Param('id', new ParseMongoIdPipe()) id: string) {
